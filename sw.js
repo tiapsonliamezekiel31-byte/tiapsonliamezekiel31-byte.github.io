96d1a45294e3bfc62bb5aff9cdabca2cc6fcc87c
tiapsonliamezekiel31-byte <tiapsonliamezekiel31@gmail.com>
2026-05-28 14:41:34 +0800

    Hotfix sw.js: remove conflict marker and bump cache to v6

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nemesis-ultimate-roguelike-v5';
+const CACHE_NAME = 'nemesis-ultimate-roguelike-v6';
 const APP_SHELL = [
   './',
   './index.html',
@@ -73,4 +73,3 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
->>>>>>> origin/main
