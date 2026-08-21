5aa56ff1e18bdcd8033cc317459546e63405dca3
tiapsonliamezekiel31-byte <tiapsonliamezekiel31@gmail.com>
2026-08-21 22:30:27 +0800

    ci: bump service worker cache to 20260821-2230

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nemesis-ultimate-roguelike-20260821-warm-paper';
+const CACHE_NAME = 'nemesis-ultimate-roguelike-20260821-2230';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
